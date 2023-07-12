858f21c6ebe19bfd5b49b9cc8f1e2bf1406d3449
mohangandhi.vadapall <mohangandhi.vadapalli@acsicorp.com>
2023-07-12 17:15:25 +0530

    in TimeSlotController , posting employee time slots it gives 400 error.once check it

--- src/main/java/com/example/demo/api/EmployeeController.java
+++ src/main/java/com/example/demo/api/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.api;
 
+import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,5 +1,6 @@
-package com.example.demo.api;
+package com.example.demo.repository;
 
+import com.example.demo.api.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
